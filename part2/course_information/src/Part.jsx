0d5ad50284de9c0c6564b3e3820df634631d54bf
irene <wunder.frau@yandex.by>
2024-12-03 01:43:33 +0200

    prt02: 2.4: Course information step 9 add number of exercises

--- part2/course_information/src/Part.jsx
+++ part2/course_information/src/Part.jsx
@@ -1,2 +1,10 @@
-const Part =({course})=> <div>{course.name}</div>
+const Part = ({ course }) => {
+    return (
+      <div>
+        <span className="part-name">{course.name}</span>
+        <span>{course.exercises}</span>
+      </div>
+    );
+  };
+  
 export default Part;
