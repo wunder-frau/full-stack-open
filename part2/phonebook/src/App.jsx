c1d73d26a2ef75199e60af49cb1dfb863bf59269
irene <wunder.frau@yandex.by>
2024-12-07 15:04:53 +0200

    add Error component

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,12 +4,20 @@ import InputField from './InputField.jsx'
 import PersonForm from './PersonForm.jsx'
 import Persons from './Persons.jsx'
 import personsService from './personsService.jsx'
-
+import Error from './Error.jsx'
 function App() {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [inputFilter, setInputFilter] = useState('')
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  const handleError = (message) => {
+    setErrorMessage(message);
+    setTimeout(() => {
+      setErrorMessage(null);
+    }, 5000);
+  };
 
   useEffect(() => {
     personsService
@@ -18,7 +26,7 @@ function App() {
         setPersons(response)
       })
       .catch(error => {
-        console.error('Error fetching persons:', error);
+        handleError('Error fetching persons');
       });
   }, [])
 
@@ -30,20 +38,21 @@ function App() {
         setPersons(persons.filter(p => p.id !== person.id))
       })
       .catch(error => {
-        console.error('Error deleting person:', error);
+        setErrorMessage('Error deleting person:');
       });
     }
   }
 
   const handleFilterInput =(e)=> {
     setInputFilter(e.target.value)
+    handleError(`Not found.`);
   }
 
   const handleNameInput =(e)=> {
     if (/^[a-zA-Z\s]*$/.test(e.target.value)) {
       setNewName(e.target.value);
     } else {
-      console.log('Invalid input, only alphabetical characters and spaces are allowed.');
+     handleError('Invalid input, only alphabetical characters and spaces are allowed.');
     }
   }
 
@@ -58,9 +67,10 @@ function App() {
         setPersons([...persons, response])
         setNewName("")
         setNewNumber("")
+        handleError('Person is added.');
     })
     .catch(error => {
-      console.error('Error adding person:', error);
+      handleError('Error adding person.');
     });
   }
 
@@ -71,9 +81,10 @@ function App() {
         setPersons(persons.map(p => p.id === person.id ? response : p))
         setNewName("")
         setNewNumber("")
+        handleError('Number is updated.');
     })
     .catch(error => {
-      console.error('Error updating person:', error);
+      handleError('Error updating person.');
     });
   }
 
@@ -96,6 +107,7 @@ function App() {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Error message={errorMessage} />
       <InputField
         type="text"
         label='filter shown with'
