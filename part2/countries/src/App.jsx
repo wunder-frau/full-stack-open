371807445085bf72f87fa4f6047245ef133bc5f6
irene <wunder.frau@yandex.by>
2024-12-09 23:18:37 +0200

    add filter logic

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -2,15 +2,21 @@ import { useState, useEffect } from 'react'
 import './App.css'
 import countryService from './countryService.jsx';
 import Countries from './Countries.jsx';
+import InputField from './InputField.jsx';
 
 function App() {
   const [countries, setCountries] = useState([]);
+  const [inputFilter, setInputFilter] = useState('');
+
+
+  const handleFilterInput = (e) => {
+    setInputFilter(e.target.value);
+  };
 
   useEffect(() => {
     countryService
       .getAll()
       .then((response) => {
-        console.log("loook! :", response[0].name)
         setCountries(response);
       })
       .catch(error =>
@@ -18,13 +24,32 @@ function App() {
       )
   }, [])
 
+  const filteredCountries = countries.filter((c) =>
+    c.name.common.toLowerCase().includes(inputFilter.toLowerCase())
+  );
+
   return (
     <>
       <div>
       </div>
       <h1>Vite + React</h1>
+      <InputField
+        type="text"
+        label="Filter shown with"
+        value={inputFilter}
+        onChange={handleFilterInput}
+        id="filter"
+      />
       <div className="card">
-        <Countries countries={countries}/>
+        {inputFilter ? (
+          filteredCountries.length > 0 ? (
+            <Countries countries={filteredCountries} />
+          ) : (
+            <p className="notification error">Not found.</p>
+          )
+        ) : (
+          <p>Please enter a filter to search for countries.</p>
+        )}
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
